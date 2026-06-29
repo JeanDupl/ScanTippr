9e19267eaff3b84e3753f723e46217fa800a4a0b
JeanDupl <jean076592@gmail.com>
2026-06-29 13:23:18 +0000

    Test hardcoded keys

--- scantippr/lib/supabase.ts
+++ scantippr/lib/supabase.ts
@@ -1,10 +1,6 @@
 import { createClient } from '@supabase/supabase-js'
 
-const supabaseUrl = process.env.NEXT_PUBLIC_SUPABASE_URL!
-const supabaseAnonKey = process.env.NEXT_PUBLIC_SUPABASE_ANON_KEY!
-
-export const supabase = createClient(supabaseUrl, supabaseAnonKey, {
-  auth: {
-    persistSession: false
-  }
-})
+export const supabase = createClient(
+  'https://fpagcvbhxzrfrqsbcsuw.supabase.co',
+  'eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJpc3MiOiJzdXBhYmFzZSIsInJlZiI6ImZwYWdjdmJoeHpyZnJxc2Jjc3V3Iiwicm9sZSI6ImFub24iLCJpYXQiOjE3ODI1NDExODYsImV4cCI6MjA5ODExNzE4Nn0.gEYHH3whzquG1QcdcEvO5dWUbXOP4k6HiT1zDDyuSFY'
+)
