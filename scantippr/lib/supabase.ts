f81eff05e1ab47254bb872fa53f9f396926db8a9
JeanDupl <jean076592@gmail.com>
2026-06-30 14:15:33 +0000

    Move Supabase credentials to environment variables

--- scantippr/lib/supabase.ts
+++ scantippr/lib/supabase.ts
@@ -1,6 +1,6 @@
 import { createClient } from '@supabase/supabase-js'
 
 export const supabase = createClient(
-  'https://fpagcvbhxzrfrqsbcsuw.supabase.co',
-  'eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJpc3MiOiJzdXBhYmFzZSIsInJlZiI6ImZwYWdjdmJoeHpyZnJxc2Jjc3V3Iiwicm9sZSI6ImFub24iLCJpYXQiOjE3ODI1NDExODYsImV4cCI6MjA5ODExNzE4Nn0.gEYHH3whzquG1QcdcEvO5dWUbXOP4k6HiT1zDDyuSFY'
+  process.env.NEXT_PUBLIC_SUPABASE_URL!,
+  process.env.NEXT_PUBLIC_SUPABASE_ANON_KEY!
 )
